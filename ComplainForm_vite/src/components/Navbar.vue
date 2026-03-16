7cac13c9c1290831d9be31481717eb980f4d4326
Mohammed Moota <mohammedmoota123@gmail.com>
2026-03-16 11:25:55 +0800

    merge: sync with main

--- ComplainForm_vite/src/components/Navbar.vue
+++ ComplainForm_vite/src/components/Navbar.vue
@@ -1,17 +1,11 @@
-<<<<<<< HEAD
-=======
 <!-- /* just try */ -->
 
->>>>>>> 3eef161 (feat: just try)
 <script setup lang="ts">
 
 </script>
 
 
 <template>
-<<<<<<< HEAD
     
-=======
 
->>>>>>> 3eef161 (feat: just try)
 </template>

0c89f1077182506b6462bae35a3b53b838eac87b
MUHAMMAD IDLAN BIN IDRIS <idlan9114@gmail.com>
2026-03-16 11:15:46 +0800

    Update Navbar.vue

--- ComplainForm_vite/src/components/Navbar.vue
+++ ComplainForm_vite/src/components/Navbar.vue
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-=======
-<!-- /* just try */ -->
 
->>>>>>> 3eef161 (feat: just try)
 <script setup lang="ts">
 
 </script>
 
 
 <template>
-<<<<<<< HEAD
-    
-=======
 
->>>>>>> 3eef161 (feat: just try)
 </template>

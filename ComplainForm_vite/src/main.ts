68827043decac2a2e3a23731c2ffa960a0862e97
idlan9114 <idlan9114@gmail.com>
2026-03-16 10:16:10 +0800

    feat: edit dependencies

--- ComplainForm_vite/src/main.ts
+++ ComplainForm_vite/src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 
+
+const app = createApp(App)
+
+app.use(createPinia())
 createApp(App).mount('#app')
--- ComplainForm_vite/src/view/Form.vue
+++ ComplainForm_vite/src/view/Form.vue
@@ -3,8 +3,25 @@
 </script>
 
 <template>
- <main class="bg-black w-screen h-screen">
-    ewewe
+ <main class="w-screen h-screen ">
+    <div>
+    <form action="">
+         <h3>Student Complain Form</h3>
+        <Label for="title">ID: </Label>
+        <input type="text" name="title" placeholder="Enter a title..."><br />
+
+        <Label for="title">Name: </Label>
+        <input type="text" name="title" placeholder="Enter a title..."><br />
+
+        <Label for="title">Email: </Label>
+        <input type="text" name="title" placeholder="Enter a title..."><br />
+
+        <Label for="description">Description *</Label>
+        <textarea name="description" rows="4" placeholder="Enter a description..." /><br />
+        <button  class="btn gray">Submit</button>
+    </form>
+    </div>
+    
 
 
  </main>
